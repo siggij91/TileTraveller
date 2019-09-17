4ed937de0f87b485cd87e03b0c698ddf7b93c16f
Kristjan Mar Kjartansson <krissikja@gmail.com>
2019-09-17 09:52:08 +0000

    2nd commit

--- tile_traveller.py
+++ tile_traveller.py
@@ -6,3 +6,21 @@
 #E = +1 in first index and W = -1
 #Once in new tile, show the allowable moves
 #Once player enters 3, 1 show them that they win.
+
+SQ11 = 'N'
+SQ12 = 'NES'
+SQ13 = 'ES'
+SQ21 = 'N'
+SQ22 = 'WS'
+SQ23 = 'WE'
+SQ31 = 'N'
+SQ32 = 'NS'
+SQ33 = 'WS'
+
+current_square = SQ11
+
+while current_square != SQ31:
+    can_move()
+    select_move()
+else:
+    print("Victory!")
