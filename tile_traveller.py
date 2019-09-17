4a7aaebe0eaaff3ae1e9821ad063dfbe9e40d44a
Sigurður Jakobsson <iceman13_8@hotmail.com>
2019-09-17 09:15:58 +0000

    Algorithm for tile traveller

---
+++ tile_traveller.py
@@ -0,0 +1,8 @@
+##Project 5 Tile Traveler
+
+#Create tile structure and label it
+#Append allowable moves to file structure
+#N = +1 in second index and S = -1
+#E = +1 in first index and W = -1
+#Once in new tile, show the allowable moves
+#Once player enters 3, 1 show them that they win.
